add83b315cf3487f233eaed44e7dc24f49901a8f
ipanga <ipanga@outlook.fr>
2025-10-13 09:29:23 +0300

    Signed app for Android Play Store export

--- android/app/src/main/kotlin/com/example/foot_rdc/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.foot_rdc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/tootiyesolutions/footrdc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tootiyesolutions.footrdc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
